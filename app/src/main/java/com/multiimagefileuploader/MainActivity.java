2e91a94f6f202b5d76733b2eb91508719d0ee591
joyal <joyal670@gmail.comgit config --listgit config --global user.email joyal670@gmail.com>
2020-12-19 20:31:57 +0530

    Initial commit

---
+++ app/src/main/java/com/multiimagefileuploader/MainActivity.java
@@ -0,0 +1,134 @@
+package com.multiimagefileuploader;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity
+{
+    Button chooseBtn, uploadBtn;
+    TextView alertTxt;
+    private ArrayList<Uri> ImageList = new ArrayList<Uri>();
+    private int uploads = 0;
+    private DatabaseReference databaseReference;
+    private ProgressDialog progressDialog;
+    int index = 0;
+    public static final int PICK_IMAGE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("User_one");
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Uploading ..........");
+
+        alertTxt = findViewById(R.id.alertTxt);
+        chooseBtn = findViewById(R.id.chooseBtn);
+        uploadBtn = findViewById(R.id.uploadBtn);
+
+        chooseBtn.setOnClickListener(v ->
+        {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            startActivityForResult(intent, PICK_IMAGE);
+        });
+
+        uploadBtn.setOnClickListener(v ->
+        {
+            upload();
+        });
+    }
+
+    private void upload()
+    {
+        alertTxt.setText("Please Wait ... If Uploading takes Too much time please the button again ");
+        progressDialog.show();
+        final StorageReference ImageFolder =  FirebaseStorage.getInstance().getReference().child("ImageFolder");
+        for (uploads=0; uploads < ImageList.size(); uploads++) {
+            Uri Image  = ImageList.get(uploads);
+            final StorageReference imagename = ImageFolder.child("image/"+Image.getLastPathSegment());
+
+            imagename.putFile(ImageList.get(uploads)).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    imagename.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+
+                            String url = String.valueOf(uri);
+                            SendLink(url);
+                        }
+                    });
+
+                }
+            });
+
+
+        }
+    }
+
+    private void SendLink(String url)
+    {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("link", url);
+        databaseReference.push().setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                progressDialog.dismiss();
+                alertTxt.setText("Image Uploaded Successfully");
+                uploadBtn.setVisibility(View.GONE);
+                ImageList.clear();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                if (data.getClipData() != null) {
+                    int count = data.getClipData().getItemCount();
+
+                    int CurrentImageSelect = 0;
+
+                    while (CurrentImageSelect < count) {
+                        Uri imageuri = data.getClipData().getItemAt(CurrentImageSelect).getUri();
+                        ImageList.add(imageuri);
+                        CurrentImageSelect = CurrentImageSelect + 1;
+                    }
+                    alertTxt.setVisibility(View.VISIBLE);
+                    alertTxt.setText("You Have Selected "+ ImageList.size() +" Pictures" );
+                    chooseBtn.setVisibility(View.GONE);
+                }
+            }
+        }
+    }
+}
